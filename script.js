4386c02703bacb55de661a5ad76e18d4d0b72024
Haider Ali <devhaiderale@gmail.com>
2025-09-24 15:15:48 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+const cursor = document.querySelector(".cursor-ball");
+
+document.addEventListener("mousemove", (e) => {
+    cursor.style.top = e.clientY + "px";
+    cursor.style.left = e.clientX + "px";
+});
+9
+
+const container = document.getElementById("cardContainer");
+const nextBtn = document.getElementById("nextBtn");
+
+let currentPage = 0;
+const cardsPerPage = 3;
+const totalCards = container.children.length;
+const totalPages = Math.ceil(totalCards / cardsPerPage);
+
+nextBtn.addEventListener("click", () => {
+    currentPage = (currentPage + 1) % totalPages;
+    const offset = -(currentPage * 930); // 3 cards * (280px + gap 25px)
+    container.style.transform = `translateX(${offset}px)`;
+});
+
+
+
+
+
+
+
+
 
